d8c924709556e58c10121db1cf125067bbb6cb32
Andre Granemann <andre@zuplae.com>
2023-04-13 21:27:02 -0300

    Sunbindo arquivo para o andre

--- Exercícios/exercício01.py
+++ Exercícios/exercício01.py
@@ -1,7 +1,9 @@
+#Variaveis com dados fixos 
 nome = 'Andre'
 sobrenome = 'Campos'
 idade = '38'
 cor = 'branco'
 sexo = 'masculino'
 
+#print simples para exibir dados atribuidos 
 print(nome, sobrenome, idade, cor, sexo)
--- Exercícios/exercício02.py
+++ Exercícios/exercício02.py
@@ -1,7 +1,8 @@
+# parcialmente correto remover os tipos primitivos
 nome = str(input("Digite seu nome>> "))
 sobrenome = str(input("Digite seu sobrenome>> "))
 idade = str(input("Digite sua idade>> "))
 raça = str(input("Digite sua raça>> "))
 sexo = str(input("Digite seu sexo>> "))
-
+#imprimindo simplificadamente
 print(nome, sobrenome, idade, raça, sexo)
